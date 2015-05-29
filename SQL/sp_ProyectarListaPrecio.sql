4694a8f6802de8114b4466d1655bdaed6a3fe7d6
Mario Naranjo <marionaranjo12@gmail.com>
2015-05-29 11:45:53 -0600

    Versión 1.1

--- SQL/sp_ProyectarListaPrecio.sql
+++ SQL/sp_ProyectarListaPrecio.sql
@@ -10,12 +10,22 @@ GO
 -- =============================================
 CREATE PROCEDURE sp_proyectarListaPrecio
 	-- Parámetros
+	@pCodListaPrecio VARCHAR(50),
+	@pCantidadMeses INT
 	
 AS
 BEGIN
 	
 	SET NOCOUNT ON;
 
+	DECLARE @mesProyectado INT,
+			@codigoArticulo VARCHAR(20),
+			@precio FLOAT(5) --El precio se espera en dolares
+	SET @precio = 0.0;
+	SET @mesProyectado = MONTH(GETDATE()) + @pCantidadMeses;
+
+	/*Cursor con los artículos contenidos en la lista de precios*/
+	
     
 END
 GO
