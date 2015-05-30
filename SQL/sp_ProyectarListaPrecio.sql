381cd0cf53f45fa292ea2fb9e74c61aaae3ec076
Mario Naranjo <marionaranjo12@gmail.com>
2015-05-30 12:37:11 -0600

    Versión 1.3
    Proyección de la lista de precios -para artículos sin componentes

--- SQL/sp_ProyectarListaPrecio.sql
+++ SQL/sp_ProyectarListaPrecio.sql
@@ -20,12 +20,27 @@ BEGIN
 
 	DECLARE @mesProyectado INT,
 			@codigoArticulo VARCHAR(20),
-			@precio FLOAT(5) --El precio se espera en dolares
+			@precio FLOAT(5), --El precio se espera en dolares
+			@tipoCambio FLOAT(3)
 	SET @precio = 0.0;
+	SET @tipoCambio = 0.0;
 	SET @mesProyectado = MONTH(GETDATE()) + @pCantidadMeses;
 
 	/*Cursor con los artículos contenidos en la lista de precios*/
-	
+	DECLARE articulosListaPrecio CURSOR STATIC LOCAL FOR
+		SELECT d.codigoArticulo, d.precio FROM DetalleLP d
+		WHERE d.codigoListaPrecio = @pCodListaPrecio
+
+	/*Seleccionar el tipo de cambio para el mes al que se desea hacer la proyección*/
+	SELECT @tipoCambio = t.valor FROM TipoDeCambio t
+	WHERE MONTH(t.fecha) = @mesProyectado AND t.esProyectado = 0 AND t.FK_moneda1 = 1 AND t.FK_moneda2 = 2
+
+	OPEN articulosListaPrecio
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		FETCH NEXT FROM articuloListaPrecio INTO @codigoArticulo, @precio
+		INSERT INTO Proyecciones VALUES(GETDATE(), DATEADD(MONTH, @pCantidadMeses), @precio * @tipoCambio, @codigoArticulo, 1)
+	END
     
 END
 GO
