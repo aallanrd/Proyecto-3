342ce767d3b5eafc4322eb5820b24b03924afee2
Allan Rojas <allanjose91@gmail.com>
2015-05-31 13:38:36 -0500

    Version 2

--- SQL/Procedures/listar_ComponentesxArticulo.sql
+++ SQL/Procedures/listar_ComponentesxArticulo.sql
@@ -1,19 +1,7 @@
 
  use proyecto;
 
- Select * from Articulos;
- go
- Select * from Componentes;
- go
- Delete from   componentes where codigo = 'Abc';
- go
- go
- Insert into   componentes values ('ABBE8388');
---/////////////////////////////////////////////////////////////////////////
-
- Insert into  componentesArticulos values ('8ABB17C7',2);
-
- select * from componentesArticulos;
+ 
 
  --drop procedure listar
 Create Procedure listar (@codArt varchar(20))
@@ -29,5 +17,3 @@ ON Articulos.codigoArticulo = ComponentesArticulos.codigoArt
 
 where idArticulo = @codArt;
 End
-
-exec listar '8ABB17C7'
